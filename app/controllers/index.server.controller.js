746d2548cb43f8d4d0e4d41e211663b8b038a1d2
augusteen <austin.chellian@gmail.com>
2015-07-22 21:08:58 +0530

    init load
    initial load

---
+++ app/controllers/index.server.controller.js
@@ -0,0 +1,11 @@
+exports.render = function(req, res) {
+    //res.send('Hello World');
+    if (req.session.lastVisit) {
+        console.log(req.session.lastVisit);
+    }
+    req.session.lastVisit = new Date();
+    
+    res.render('index', {
+        title: 'Node js first app'
+    });
+};
