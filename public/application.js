0098ee4796056aaf54da4f2442771e011bece90f
augusteen <austin.chellian@gmail.com>
2015-07-23 21:07:30 +0530

    ddd
    dd

--- public/application.js
+++ public/application.js
@@ -1,7 +1,15 @@
 var mainAppName = 'myApp';
 
-var mainApp = angular.module(mainAppName,['example']);
+var mainApp = angular.module(mainAppName, ['example']);
 
-angular.element(document).ready(function(){
-	angular.bootstrap(document,[mainAppName]);
+var mainApp = angular.module(mainAppName, ['ngRoute', 'example']);
+
+
+mainApp.config(['$locationProvider',
+    function($locationProvider) {
+        $locationProvider.hashPrefix('!');
+    }
+]);
+angular.element(document).ready(function() {
+    angular.bootstrap(document, [mainAppName]);
 });
