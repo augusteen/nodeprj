c71bc14955693b8e37930cb161a3db31bd5210a6
augusteen <austin.chellian@gmail.com>
2015-07-24 17:27:13 +0530

    changing port

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('./config/express');
 
 var app = express();
-app.listen(3000);
+app.listen(8787);
 
 module.exports = app;
